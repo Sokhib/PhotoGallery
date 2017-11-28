f310ad0015803965bbd00a4dfbf9623c2184ff3b
Sokhib <sokhibsaid@gmail.com>
2017-11-28 18:16:20 +0300

    adding Pollservice class

---
+++ app/src/main/java/com/example/aspirev7/photogallery/PollService.java
@@ -0,0 +1,86 @@
+package com.example.aspirev7.photogallery;
+
+import android.app.AlarmManager;
+import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
+import android.net.ConnectivityManager;
+import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+/**
+ * Created by AspireV7 on 11/28/2017.
+ */
+
+public class PollService extends IntentService {
+    private static final String TAG = "PollService";
+    private static final int POLL_INTERVAL = 1000 * 5 * 60;
+
+
+    public PollService() {
+        super(TAG);
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        ConnectivityManager cManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        boolean isNetworkAvailable = cManager.getActiveNetworkInfo() != null;
+        if (!isNetworkAvailable) return;
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String query = prefs.getString(FlickrFetchr.PREF_SEARCH_QUERY, null);
+        String lastResultId = prefs.getString(FlickrFetchr.PREF_LAST_RESULT_ID, null);
+        ArrayList<GalleryItem> items;
+        if (query != null) {
+            items = new FlickrFetchr().search(query);
+        } else items = new FlickrFetchr().fetchItems();
+        if (items.size() == 0)
+            return;
+
+        String resultId = items.get(0).getId();
+        if (!resultId.equals(lastResultId))
+            Log.i(TAG, "Got a new result: " + resultId);
+        Resources r = getResources();
+        PendingIntent pi = PendingIntent.getActivity(this, 0, new Intent(this, PhotoGalleryActivity.class), 0);
+        Notification mNotification = new NotificationCompat.Builder(this)
+                .setTicker(r.getString(R.string.new_pictures_title))
+                .setSmallIcon(android.R.drawable.ic_menu_report_image)
+                .setContentTitle(r.getString(R.string.new_pictures_title))
+                .setContentText(r.getString(R.string.new_pictures_text))
+                .setContentIntent(pi)
+                .setAutoCancel(true)
+                .build();
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(0,mNotification);
+
+
+        prefs.edit().putString(FlickrFetchr.PREF_LAST_RESULT_ID, resultId).commit();
+    }
+
+
+    public static void setServiceAlarm(Context c, boolean isOn) {
+        Intent intent = new Intent(c, PollService.class);
+        PendingIntent pendingIntent = PendingIntent.getService(c, 0, intent, 0);
+        AlarmManager alarmManager = (AlarmManager) c.getSystemService(Context.ALARM_SERVICE);
+        if (isOn)
+            alarmManager.setRepeating(AlarmManager.RTC, System.currentTimeMillis(), POLL_INTERVAL, pendingIntent);
+        else {
+            alarmManager.cancel(pendingIntent);
+            pendingIntent.cancel();
+        }
+    }
+
+    public static boolean isServiceAlarmOn(Context context) {
+        Intent intent = new Intent(context, PollService.class);
+        PendingIntent pi = PendingIntent.getService(context, 0, intent, PendingIntent.FLAG_NO_CREATE);
+        return pi != null;
+    }
+}
